ff26ec2f2a9f8dae1d1dd080fd13bb6ce41c39fe
JeoninRiverWater <128760945+JeoninRiverWater@users.noreply.github.com>
2026-03-04 19:31:00 +0900

    refactor: improve algorithm that make sudoku

--- 03_sudoku.py
+++ 03_sudoku.py
@@ -3,53 +3,59 @@ import random
 # br: block row, bc: block column
 # cx: current x
 # cy: current y
-def make(br, bc, cx = 0, cy = 0, board = None, seed = 42) : 
+def make(br, bc, r = 0, c = 0, board = None, seed = 42,
+         row = None, col = None, block = None) : 
 
     # n: size of sudoku(n*n size), max number(1~n)
-    n = br*bc
-    if board == None :
+    n = br * bc
+    if board is None : 
         random.seed(seed)
         board = [[0 for _ in range(n)] for _ in range(n)]
+        row = [set() for _ in range(n)]
+        col = [set() for _ in range(n)]
+        block = [set() for _ in range(n)]
 
-    if cy == n : 
+    if c == n : 
         return board
-
     
-    ## 이곳에 스도쿠 생성 규칙을 포함
-    # 같은 열에 같은 숫자 X
-    rule_row = set(board[cx])
-    # 같은 행에 같은 숫자 X
-    rule_col = set([row[cy] for row in board])
-    # 같은 블록에 같은 숫자 X
-    # 블록의 시작 좌표(sx, sy)
-    sc = (cy//bc)*bc
-    sr = (cx//br)*br
-    rule_blk = {
-        board[r][c]
-        for r in range(sr, sr + br)
-        for c in range(sc, sc + bc)
-    }
+    block_idx = (r // br) * (n // bc) + (c // bc)
 
-    available = list({i for i in range(1, n + 1)} - (rule_row | rule_col | rule_blk))
-    random.shuffle(available)
+    available = list(
+        set(range(1, n+1))
+        - row[r]
+        - col[c]
+        - block[block_idx]
+    )
 
     if not available : 
-        return
+        return False
+    
+    random.shuffle(available)
     
     for num in available : 
-        board[cx][cy] = num
-        if cx == n-1 : 
-            result = make(br, bc, 0, cy+1, board)
+        board[r][c] = num
+        row[r].add(num)
+        col[c].add(num)
+        block[block_idx].add(num)
+
+        if r == n-1 : 
+            result = make(br, bc, 0, c+1, board, seed,
+                          row, col, block)
         else : 
-            result = make(br, bc, cx+1, cy, board)
+            result = make(br, bc, r+1, c, board, seed,
+                          row, col, block)
         if result : 
             return result
         
-        board[cx][cy] = 0
+        board[r][c] = 0
+        row[r].remove(num)
+        col[c].remove(num)
+        block[block_idx].remove(num)
+        
 
     return False
 
 # test code: make 4*4 sudoku
-board = make(3, 3)
+board = make(2, 3)
 for i in board : 
     print(*i)
