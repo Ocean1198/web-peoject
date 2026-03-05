f286d91a69dd19a6a0af603b64121be15144be88
JeoninRiverWater <128760945+JeoninRiverWater@users.noreply.github.com>
2026-03-05 15:38:42 +0900

    refactor: merge temporary files into one and add comments

--- 03_sueoku_generator.py
+++ 03_sueoku_generator.py
@@ -1,9 +1,48 @@
+"""    
+Sudoku Generator
+
+스도쿠의 블록 크기와 난이도를 바탕으로 스도쿠 문제와 정답을 생성합니다.
+
+1. make_ans : 완성된 스도쿠 생성
+2. solver   : 스도쿠 풀이, 해답 개수 확인
+3. generator: 난이도 기반의 스도쿠 문제 생성
+
+난이도의 구분:
+- 쉬움(0): 
+    ~ 40%의 힌트 개수, 1000회 이하의 탐색 횟수
+- 보통(1):
+    ~ 35%의 힌트 개수, 10000회 이하의 탐색 횟수
+- 어려움(2) : 
+    ~ 30%의 힌트 개수, 50000회 이하의 탐색 횟수
+- 극한(3) : 
+    ~ 25%의 힌트 개수, 100000회 이하의 탐색 횟수
+
+지원되는 스도쿠 크기:
+- br * bc 블록 스도쿠
+- 예: 3*3 >> 9*9 스도쿠
+- 예: 2*3 >> 6*6 스도쿠
+"""
+
 import random
 
-# make_ans: 정답 생성 함수. 블록 크기(br, bc)를 입력받아 정답(2차원 리스트)을 출력한다.
 def make_ans(br, bc) : 
+    """
+    완전한 스도쿠를 만듭니다.
 
-    # n: size of sudoku(n*n size), max number(1~n)
+    
+    입력
+    ----------
+    br: int
+        스도쿠 내 블록들의 행(row)
+    bc: int
+        스도쿠 내 블록들의 열(column)
+
+    출력
+    ----------
+    board: list[list[int]]
+        n*n 크기의 완전한 스도쿠
+    """
+
     n = br * bc
 
     board = [[0 for _ in range(n)] for _ in range(n)]
@@ -15,7 +54,7 @@ def make_ans(br, bc) :
         if c == n : 
             return True
         
-        block_idx = (r // br) * br + (c // bc)
+        block_idx = (r // br) * bc + (c // bc)
 
         available = list(
             set(range(1, n+1))
@@ -54,6 +93,25 @@ def make_ans(br, bc) :
 
 # solver: 스도쿠 풀이 함수. 블록 크기(br, bc)와 스도쿠를 입력받아 해답의 개수와 탐색 횟수를 출력한다.
 def solver(board, br, bc) : 
+    """
+    주어진 스도쿠를 풀이하고, 해의 개수를 반환합니다.
+
+    입력
+    ----------
+    board: list[list[int]]
+        풀이할 스도쿠 문제(n*n 크기)
+    br: int
+        스도쿠 내 블록들의 행(row)
+    bc: int
+        스도쿠 내 블록들의 열(column)
+
+    출력
+    ----------
+    sol: int
+        스도쿠 해의 갯수
+    backtrack: int
+        정답 탐색 횟수
+    """
 
     n = br * bc
     sol = 0
@@ -62,7 +120,7 @@ def solver(board, br, bc) :
     row = [set(board[i]) for i in range(n)]
     col = [set([r[i] for r in board]) for i in range(n)]
     block = [set() for _ in range(n)]
-    block_idx = [(r // br) * br + (c // bc)
+    block_idx = [(r // br) * bc + (c // bc)
              for r in range(n)
              for c in range(n)]
     
@@ -86,7 +144,7 @@ def solver(board, br, bc) :
             return sol
         
         r, c = empty_cells[index]
-        b = (r // br) * br + (c // bc)
+        b = (r // br) * bc + (c // bc)
 
         # 규칙 검사
         available = list(
@@ -116,21 +174,31 @@ def solver(board, br, bc) :
 # generate: 스도쿠 문제 생성 함수. 블록 크기(br, bc)와 난이도를 입력받아 문제(2차원 리스트)를 출력한다.
 def generate(br, bc, level, seed = 42) : 
     """
-    기본 진행
-    1. 완성본을 만든다. 
-    2. 완성본에서 하나씩 숫자를 랜덤으로 지운다. 
-    3. 일부 지워진 완성본을 solver로 풀이한다. 
-    4. 2-3을 반복하며 난이도에 따른 조건 중 하나를 만족하면 문제를 내보낸다.
-        조건1. 힌트 개수의 비율(전체 대비 20~40%)
-        조건2. solver의 탐색 횟수
+    n*n 크기의 스도쿠 문제를 만듭니다.
 
-    난이도는 쉬움/보통/어려움/극한 4개로만 구분하며, 다음과 같이 사용(추후 수정 가능)
-    쉬움: 40% 정도의 힌트, 1000회 이하의 탐색 횟수
-    보통: 35% 정도의 힌트, 10000회 이하의 탐색 횟수
-    어려움: 30% 정도의 힌트, 50000회 이하의 탐색 횟수
-    극한: 25% 정도의 힌트, 100000회 이하의 탐색 횟수
-    *sudoku.com의 extreme 난이도(최고 난이도) 문제 중 하나의 탐색 횟수는 198132회였다.
+    입력
+    ----------
+    br: int
+        스도쿠 내 블록의 열(row)
+    bc: int
+        스도쿠 내 블록의 행(column)
+    level: int
+        문제의 난이도
+        0: 쉬움
+        1: 보통
+        2: 어려움
+        3: 극한
+    seed: int, optional (default = 42)
+        퍼즐 생성의 seed
+
+    출력
+    ----------
+    board_ori: list[list[int]]
+        완전한 스도쿠
+    board: list[list[int]]
+        빈칸이 0인 스도쿠 문제
     """
+
     random.seed(seed)
 
     # board: n*n size list
